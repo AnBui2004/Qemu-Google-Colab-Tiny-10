4c00cf91bfe2d4712385abbcad3d1a37eaea0f79
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-25 22:12:40 +0700

    Update Run2.sh

--- Run2.sh
+++ Run2.sh
@@ -2,17 +2,17 @@ sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
-qemu-img create -f raw windows7nano.img 64G
+qemu-img create -f raw Tiny10.img 64G
 wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
-wget -O windows7_super-nano_lite.iso 'https://dl.malwarewatch.org/windows/mods/Tiny%2010.iso'
+wget -O Tiny10.iso 'https://dl.malwarewatch.org/windows/mods/Tiny%2010.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=windows7_super-nano_lite.iso,media=cdrom \
-  -drive file=windows7nano.img,format=raw \
+  -drive file=Tiny10.img.iso,media=cdrom \
+  -drive file=Tiny10.img,format=raw \
   -drive file=RTL8139F.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
