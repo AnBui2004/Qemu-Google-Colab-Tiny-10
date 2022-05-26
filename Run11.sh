13c8545e2b89ed3ee3b54965c4f09e4b1b3ec811
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-26 15:32:01 +0700

    Create Run11.sh

---
+++ Run11.sh
@@ -0,0 +1,20 @@
+sudo apt-get update -y
+sudo apt-get install -y qemu-system-x86-64
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O Tiny10.qcow2 'https://archive.org/download/tiny-10_202205/Tiny10.qcow2'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=Tiny10.qcow2 \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu n270 \
+  -smp sockets=1,cores=4,threads=2 \
+  -vga std \
+  -device e1000,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
