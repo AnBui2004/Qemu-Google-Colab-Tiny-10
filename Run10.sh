5587ed2d07f28e2be1d04f2a638f22cbcd2a348b
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-05-26 11:39:57 +0700

    Update Run10.sh

--- Run10.sh
+++ Run10.sh
@@ -1,2 +1,21 @@
 sudo apt-get update -y
 sudo apt-get install -y qemu-system-x86-64
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O Tiny10.qcow2 'https://download1082.mediafire.com/pzsvnztp7nfg/rdpfg03d75nh58g/Tiny10.qcow2'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=d \
+  -drive file=Tiny10.qcow2,format=raw \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu n270 \
+  -smp sockets=1,cores=4,threads=2 \
+  -vga std \
+  -device e1000,netdev=n0 -netdev user,id=n0 \
+  -device ac97 \
+  -accel tcg,thread=multi \
