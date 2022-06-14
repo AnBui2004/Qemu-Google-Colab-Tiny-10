f6871f58136a92665e49d9fb587e6b6014a85680
An Bui <91354810+AnBui2004@users.noreply.github.com>
2022-06-14 13:16:06 +0700

    Update Run3.sh

--- Run3.sh
+++ Run3.sh
@@ -3,13 +3,13 @@ sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=Tiny10.qcow2 \
+  -drive file=Tiny10.iso,media=cdrom \
+  -drive file=Tiny10.img,format=raw \
   -drive file=RTL8139F.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -cpu n270 \
-  -smp sockets=1,cores=4,threads=2 \
-  -vga std \
-  -device e1000,netdev=n0 -netdev user,id=n0 \
+  -cpu core2duo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
